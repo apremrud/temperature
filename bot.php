37dd20e8aaaedf559b991b881d278dbc3ee4e170
manitgithub <manit.pbac@gmail.com>
2016-12-07 10:15:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -64,4 +64,5 @@ $json_array = file_get_contents("https://api.thingspeak.com/channels/83655/feed.
 //$json_array = iconv('UTF-16', 'UTF-8', $json_array);
 $json_data=json_decode($json_array,true);
 print_r($json_data);
+echo $json_data['feeds']['0']['field1'];
 
